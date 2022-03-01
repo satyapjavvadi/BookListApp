c5ad394e896cd369bf9ab47ca76b6577f3aa53a9
Satya Javvadi <saphin.rao@outlook.com>
2022-02-28 23:00:45 -0800

    Add project files.

---
+++ BookListApp/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListApp.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGetAsync()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
